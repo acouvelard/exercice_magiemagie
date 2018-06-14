700e43eff79cafa845abf7cbdda2b42bb2e5c8ad
Administrateur <Administrateur@GSVL.formation.local>
2018-06-14 11:39:15 +0200

    Entity mapped ok

---
+++ src/atos/magie_magie/entity/Joueur.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atos.magie_magie.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Administrateur
+ */
+@Entity
+public class Joueur implements Serializable {
+
+    public enum EtatJoueur {N_A_PAS_LA_MAIN, A_LA_MAIN, SOMMEIL_PROFOND, PERDU};
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Enumerated(EnumType.STRING)
+    private EtatJoueur etat;
+    
+    @Column(unique = true)
+    private String pseudo;
+    
+    private String avatar;
+       
+    @Column(nullable = false)
+    private long nbPartiesGagnees;
+    
+    @Column(nullable = false)
+    private long nbPartiesJouees;
+   
+    @OneToMany (mappedBy = "joueurProprietaire")
+    private List<Carte> cartes = new ArrayList<>();
+    
+    @ManyToOne
+    @JoinColumn
+    private Partie partieActuelle;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Joueur)) {
+            return false;
+        }
+        Joueur other = (Joueur) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "atos.magie_magie.entity.Joueur[ id=" + id + " ]";
+    }
+    
+}
